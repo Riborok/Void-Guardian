f6f306df479e6332f036bf98a9e45cc34f88af82
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2023-12-21 19:13:29 +0300

    minor updates

--- coursework/src/initialization/GameDataInitialization.cpp
+++ coursework/src/initialization/GameDataInitialization.cpp
@@ -22,7 +22,7 @@ inline AnimatedSpriteInfos initializeAnimatedSpriteInfos() {
     {ElementType::BULLET_IMPACT, {{{FrameInfoContainer{{{{4, 60}, 15}}},
         "./AppData/img/bullets/Bullet_impact_", 5}}, {0, 5}}},
     {ElementType::CHARACTER_DYING, {{{FrameInfoContainer{{{{15, 40}, 3}}},
-        "./AppData/img/wraiths/Wraith_dying_", 5}}}},
+        "./AppData/img/characters/Character_dying_", 5}}}},
     };
 }
 
